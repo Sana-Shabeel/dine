bd2bc649572099acf850c4695dee0470034c6c30
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-03-21 23:49:36 +0300

    Extend chakra theme

---
+++ src/chakra/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#FF3c00",
+    },
+  },
+});
