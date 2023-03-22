cae78766b4bf0cf38b624398796aeb2c590c8c9c
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-03-22 23:04:08 +0300

    Add theme

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -4,13 +4,11 @@ import { Button } from "./button";
 // 2. Extend the theme to include custom colors, fonts, etc
 export const theme = extendTheme({
   colors: {
-    brand: {
-      beaver: "#9e7f66",
-      codgray: "#111111",
-      mirage: "#17192b",
-      ebonyclay: "#242b37",
-      shuttlegray: "#5c6779",
-    },
+    beaver: "#9e7f66",
+    codgray: "#111111",
+    mirage: "#17192b",
+    ebonyclay: "#242b37",
+    shuttlegray: "#5c6779",
   },
   components: {
     Button,
