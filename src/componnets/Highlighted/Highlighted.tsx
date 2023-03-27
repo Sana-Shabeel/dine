7cd14793d9a66820bfc80270e1c98fe1fed62b88
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-03-27 17:43:12 +0300

    Add Highlighted folder

---
+++ src/componnets/Highlighted/Highlighted.tsx
@@ -0,0 +1,42 @@
+import { Flex, Box, Heading, Text, Image, Menu } from "@chakra-ui/react";
+import React from "react";
+import HighlightedMenu from "./HighlightedMenu";
+
+const Highlighted = () => {
+  return (
+    <Box bg="#000" color="#fff">
+      <Flex
+        flexDir={{ base: "column", xl: "row" }}
+        width={{ base: "20.5rem", md: "36rem" }}
+        marginInline="auto"
+      >
+        <Flex
+          flexDir={{ base: "column", xl: "row" }}
+          justifyContent="center"
+          alignItems={{ base: "center", xl: "flex-start" }}
+          textAlign="center"
+          gap="1.5rem"
+          m="4rem 0"
+        >
+          <Image
+            width="4.5rem"
+            src="/assets/patterns/pattern-divide.svg"
+            alt="pattern divide"
+          />
+          <Heading fontSize={{ base: "2rem", md: "3rem" }}>
+            A few highlights from <br /> our menu
+          </Heading>
+          <Text fontSize={{ md: "1.25rem" }}>
+            We cater for all dietary requirements, but here’s a glimpse at some
+            of our diner’s favourites. Our menu is revamped every season.
+          </Text>
+        </Flex>
+        <Flex>
+          <HighlightedMenu />
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Highlighted;
---
+++ src/componnets/Highlighted/HighlightedMenu.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Flex, Box, Heading, Text, Image } from "@chakra-ui/react";
+
+const HighlightedMenu = () => {
+  return (
+    <Flex
+      flexDir={{ base: "column", md: "row" }}
+      justifyContent="center"
+      alignItems={{ base: "center", xl: "flex-start" }}
+      gap="10px"
+    >
+      <picture>
+        <source
+          media="(min-width: 768px)"
+          srcSet="/assets/homepage/salmon-desktop-tablet.jpg 128w,
+          /assets/homepage/salmon-desktop-tablet@2x.jpg 256w"
+        />
+        <source
+          srcSet="/assets/homepage/salmon-mobile.jpg 327w,
+          /assets/homepage/salmon-mobile@2x.jpg 654w"
+        />
+        <Image src="/assets/homepage/salmon-mobile.jpg" alt="mobile" />
+      </picture>
+      <Box textAlign="center" padding="2rem 0">
+        <Heading mb="0.9rem" fontSize="1.25rem">
+          Seared Salmon Fillet
+        </Heading>
+        <Text>
+          Our locally sourced salmon served with a refreshing buckwheat summer
+          salad.
+        </Text>
+      </Box>
+    </Flex>
+  );
+};
+
+export default HighlightedMenu;
