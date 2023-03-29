d3dff9211a04c52750467b82c65e0f30237c91a4
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-03-29 08:12:14 +0300

    Add -

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,8 +21,8 @@ export default function Home() {
       </Head>
       <main className={LeagueSpartan.className}>
         <HeroBanner />
-        {/* <EnjoyablePlace />
-        <LocallySourced />
+        <EnjoyablePlace />
+        {/* <LocallySourced />
         <Highlighted /> */}
       </main>
     </>
