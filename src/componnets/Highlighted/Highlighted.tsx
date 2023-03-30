7ea09af068b541f81e78e1e08640fa91b80d1e8f
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-03-30 18:53:55 +0300

    Add -

--- src/componnets/Highlighted/Highlighted.tsx
+++ src/componnets/Highlighted/Highlighted.tsx
@@ -1,5 +1,6 @@
 import { Flex, Box, Heading, Text, Image, Menu } from "@chakra-ui/react";
 import React from "react";
+import InfoSection from "../InfoSection";
 import HighlightedMenu from "./HighlightedMenu";
 
 const Highlighted = () => {
@@ -7,31 +8,19 @@ const Highlighted = () => {
     <Box bg="#000" color="#fff">
       <Flex
         flexDir={{ base: "column", xl: "row" }}
-        // width={{ base: "20.5rem", md: "36rem" }}
+        width={{ base: "20.5rem", md: "36rem" }}
+        marginInline="auto"
+        paddingTop="3rem"
       >
-        <Flex
-          width={{ base: "20.5rem", md: "27.8rem" }}
-          flexDir={{ base: "column", xl: "row" }}
-          justifyContent="center"
-          alignItems={{ base: "center", xl: "flex-start" }}
-          textAlign="center"
-          gap="1.5rem"
-          m="4rem auto"
-        >
-          <Image
-            width="4.5rem"
-            src="/assets/patterns/pattern-divide.svg"
-            alt="pattern divide"
+        <Box width={{md:}}>
+          <InfoSection
+            header1="A few highlights from"
+            header2="our menu"
+            paragraph="We cater for all dietary requirements, but here’s a glimpse at some
+              of our diner’s favourites. Our menu is revamped every season."
           />
-          <Heading fontSize={{ base: "2rem", md: "3rem" }}>
-            A few highlights from <br /> our menu
-          </Heading>
-          <Text fontSize={{ md: "1.25rem" }}>
-            We cater for all dietary requirements, but here’s a glimpse at some
-            of our diner’s favourites. Our menu is revamped every season.
-          </Text>
-        </Flex>
-        <Flex>
+        </Box>
+        <Flex margin="3rem 0">
           <HighlightedMenu />
         </Flex>
       </Flex>
