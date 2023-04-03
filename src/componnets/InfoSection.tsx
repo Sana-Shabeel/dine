c9420a01aa9eb7cb0fc0f9a74f93d31bad27c66f
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-03 12:34:56 +0300

    Add -

--- src/componnets/InfoSection.tsx
+++ src/componnets/InfoSection.tsx
@@ -9,31 +9,35 @@ interface Props {
 
 const InfoSection = ({ header1, header2, paragraph }: Props) => {
   return (
-    <Flex
-      width={{ xl: "445px" }}
-      flexDir={{ base: "column" }}
-      justifyContent="center"
-      alignItems={{ base: "center", xl: "flex-start" }}
-      gap="1.5rem"
-    >
-      <Image
-        width="4.5rem"
-        src="/assets/patterns/pattern-divide.svg"
-        alt="pattern divide"
-      />
-      <Heading
-        textAlign={{ base: "center", xl: "start" }}
-        fontSize={{ base: "2rem", md: "3rem" }}
+    <Box width={{ md: "28.5rem" }}>
+      <Flex
+        flexDir={{ base: "column" }}
+        justifyContent="center"
+        alignItems={{ base: "center", xl: "flex-start" }}
+        gap="1.5rem"
       >
-        {header1} <br /> {header2}
-      </Heading>
-      <Text
-        textAlign={{ base: "center", xl: "start" }}
-        fontSize={{ md: "1.25rem" }}
-      >
-        {paragraph}
-      </Text>
-    </Flex>
+        <Image
+          width="4.5rem"
+          src="/assets/patterns/pattern-divide.svg"
+          alt="pattern divide"
+        />
+        <Heading
+          fontFamily="inherit"
+          textAlign={{ base: "center", xl: "start" }}
+          fontSize={{ base: "2rem", md: "3rem" }}
+          lineHeight={{ base: "2rem", md: "3rem" }}
+          letterSpacing="-0.5px"
+        >
+          {header1} <br /> {header2}
+        </Heading>
+        <Text
+          textAlign={{ base: "center", xl: "start" }}
+          fontSize={{ md: "1.25rem" }}
+        >
+          {paragraph}
+        </Text>
+      </Flex>
+    </Box>
   );
 };
 
