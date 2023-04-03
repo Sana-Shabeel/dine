22e5dbacea351dc47b7868cdd3b02be9da6d4fe3
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-03 11:36:26 +0300

    Add -

--- src/componnets/Highlighted/HighlightedMenu.tsx
+++ src/componnets/Highlighted/HighlightedMenu.tsx
@@ -11,18 +11,20 @@ const HighlightedMenu = () => {
       gap="10px"
       marginInline="auto"
     >
-      <picture>
-        <source
-          media="(min-width: 768px)"
-          srcSet="/assets/homepage/salmon-desktop-tablet.jpg 128w,
-          /assets/homepage/salmon-desktop-tablet@2x.jpg 256w"
-        />
-        <source
-          srcSet="/assets/homepage/salmon-mobile.jpg 327w,
-          /assets/homepage/salmon-mobile@2x.jpg 654w"
-        />
-        <Image src="/assets/homepage/salmon-mobile.jpg" alt="mobile" />
-      </picture>
+      <Flex>
+        <Box width={{ base: "" }}>
+          <source
+            media="(min-width: 768px)"
+            srcSet="/assets/homepage/salmon-desktop-tablet.jpg 128w,
+            /assets/homepage/salmon-desktop-tablet@2x.jpg 256w"
+          />
+          <source
+            srcSet="/assets/homepage/salmon-mobile.jpg 327w,
+            /assets/homepage/salmon-mobile@2x.jpg 654w"
+          />
+          <Image src="/assets/homepage/salmon-mobile.jpg" alt="mobile" />
+        </Box>
+      </Flex>
       <Box textAlign="center" padding="2rem 0">
         <Heading mb="0.9rem" fontSize="1.25rem">
           Seared Salmon Fillet
