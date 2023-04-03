bbfc5f3e18f6a2168a02ef3d8dacf13e2aa3e805
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-03 12:34:45 +0300

    Add -

--- src/componnets/Highlighted/Highlighted.tsx
+++ src/componnets/Highlighted/Highlighted.tsx
@@ -9,20 +9,19 @@ const Highlighted = () => {
       <Flex
         flexDir={{ base: "column", xl: "row" }}
         width={{ base: "20.5rem", md: "36rem" }}
+        alignItems={{ base: "center", xl: "flex-start" }}
         marginInline="auto"
         paddingTop="3rem"
+        border="1px solid green"
       >
-        <Box>
-          <InfoSection
-            header1="A few highlights from"
-            header2="our menu"
-            paragraph="We cater for all dietary requirements, but here’s a glimpse at some
+        <InfoSection
+          header1="A few highlights from"
+          header2="our menu"
+          paragraph="We cater for all dietary requirements, but here’s a glimpse at some
               of our diner’s favourites. Our menu is revamped every season."
-          />
-        </Box>
-        <Flex margin="3rem 0">
-          <HighlightedMenu />
-        </Flex>
+        />
+
+        <HighlightedMenu />
       </Flex>
     </Box>
   );
