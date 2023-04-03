b9a03d913b6292a33ae66bbc7d5293fda31d4c57
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-03 12:34:37 +0300

    Add -

--- src/componnets/Highlighted/HighlightedMenu.tsx
+++ src/componnets/Highlighted/HighlightedMenu.tsx
@@ -4,15 +4,16 @@ import { Flex, Box, Heading, Text, Image } from "@chakra-ui/react";
 const HighlightedMenu = () => {
   return (
     <Flex
-      width={{ base: "20.5rem", md: "36rem" }}
+      width={{ base: "20.5rem", md: "573px", xl: "1110px" }}
       flexDir={{ base: "column", md: "row" }}
       justifyContent="center"
       alignItems={{ base: "center", xl: "flex-start" }}
       gap="10px"
-      marginInline="auto"
+      margin={{ base: " 3rem auto" }}
+      border="1px solid green"
     >
       <Flex>
-        <Box width={{ base: "" }}>
+        <picture>
           <source
             media="(min-width: 768px)"
             srcSet="/assets/homepage/salmon-desktop-tablet.jpg 128w,
@@ -22,8 +23,12 @@ const HighlightedMenu = () => {
             srcSet="/assets/homepage/salmon-mobile.jpg 327w,
             /assets/homepage/salmon-mobile@2x.jpg 654w"
           />
-          <Image src="/assets/homepage/salmon-mobile.jpg" alt="mobile" />
-        </Box>
+          <Image
+            width={{ md: "128px" }}
+            src="/assets/homepage/salmon-mobile.jpg"
+            alt="mobile"
+          />
+        </picture>
       </Flex>
       <Box textAlign="center" padding="2rem 0">
         <Heading mb="0.9rem" fontSize="1.25rem">
