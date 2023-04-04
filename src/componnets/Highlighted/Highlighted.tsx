cc8f2a0d6dca8b13e1119de1b505d634c6edfdd7
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-04 15:18:21 +0300

    Add -

--- src/componnets/Highlighted/Highlighted.tsx
+++ src/componnets/Highlighted/Highlighted.tsx
@@ -5,13 +5,13 @@ import HighlightedMenu from "./HighlightedMenu";
 
 const Highlighted = () => {
   return (
-    <Box bg="#000" color="#fff">
+    <Box bg="#000" color="#fff" margin={{ base: "2rem 0 0", xl: "-2rem 0 0" }}>
       <Flex
         flexDir={{ base: "column", xl: "row" }}
         width={{ base: "20.5rem", md: "36rem", xl: "69.4rem" }}
         alignItems={{ base: "center", xl: "flex-start" }}
         marginInline="auto"
-        paddingTop="3rem"
+        paddingTop={{ base: "3rem", xl: "10%" }}
         gap="3rem"
       >
         <Box mt={{ xl: "2rem" }}>
