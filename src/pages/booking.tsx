c6eb75e57f84e9d6cc901ebd138ad6406c279f01
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-06 15:06:15 +0300

    Add form

--- src/pages/booking.tsx
+++ src/pages/booking.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { League_Spartan } from "next/font/google";
 import HeroBg from "@/componnets/Booking/HeroBg";
+import Form from "@/componnets/Booking/Form";
 
 const LeagueSpartan = League_Spartan({ subsets: ["latin"] });
 
@@ -8,6 +9,7 @@ const Booking = () => {
   return (
     <main className={LeagueSpartan.className}>
       <HeroBg />
+      <Form />
     </main>
   );
 };
