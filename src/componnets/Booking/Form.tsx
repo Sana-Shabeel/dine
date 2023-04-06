8ade3f534fa64aa07a7689c18b6e5be5b3f594d0
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-06 15:43:22 +0300

    Add form

--- src/componnets/Booking/Form.tsx
+++ src/componnets/Booking/Form.tsx
@@ -1,5 +1,14 @@
-import { Box, Button, Flex, Input, Select, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Select,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
+import AddPeople from "./AddPeople";
 
 const Form = () => {
   return (
@@ -7,26 +16,34 @@ const Form = () => {
       spacing={10}
       justifyContent="center"
       align="center"
-      width={{ base: "90%" }}
-      margin={{ base: "-10rem auto 3rem" }}
-      padding="2rem 1.5rem"
+      width={{ base: "90%", md: "33.75rem" }}
+      margin={{ base: "-10rem auto 3rem", xl: "-15rem 0 3rem 55%" }}
+      padding={{ base: "2rem 1.5rem", md: "3rem 2.5rem" }}
       boxShadow="1px 8px 20px -2px #919191"
       bg="#fff"
     >
       <Input variant="flushed" pl="8px" placeholder="Name" />
       <Input variant="flushed" pl="8px" placeholder="Email" />
 
-      <Box>
-        Pick a date
-        <Flex gap="1rem">
+      <Flex
+        flexDirection={{ base: "column", md: "row" }}
+        alignItems={{ md: "center" }}
+        gap="1rem"
+      >
+        <Text width="40%" fontSize="1.25rem">
+          Pick a date
+        </Text>
+        <Flex gap="1rem" width="50%">
           <Input variant="flushed" pl="8px" placeholder="MM" />
           <Input variant="flushed" pl="8px" placeholder="DD" />
           <Input variant="flushed" pl="8px" placeholder="YYYY" />
         </Flex>
-      </Box>
-      <Box>
-        Pick a date
-        <Flex gap="1rem">
+      </Flex>
+      <Flex gap="1rem">
+        <Text width="40%" fontSize="1.25rem">
+          Pick a time
+        </Text>
+        <Flex gap="1rem" width="50%">
           <Input variant="flushed" pl="8px" placeholder="09" />
           <Input variant="flushed" pl="8px" placeholder="00" />
           <Select variant="flushed" textAlign="center">
@@ -36,7 +53,8 @@ const Form = () => {
             <option value="PM">PM</option>
           </Select>
         </Flex>
-      </Box>
+      </Flex>
+      <AddPeople />
       <Button variant="solid">make reservation</Button>
     </Stack>
   );
