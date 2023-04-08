13fe437016316b3e30b73ef54c7a0004bad6c36e
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-08 18:11:15 +0300

    Add _

--- src/pages/booking.tsx
+++ src/pages/booking.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { League_Spartan } from "next/font/google";
 import HeroBg from "@/componnets/Booking/HeroBg";
-import Form from "@/componnets/Booking/Form";
+import Form from "@/componnets/Booking/Form/Form";
 import Footer from "@/componnets/Footer";
 
 const LeagueSpartan = League_Spartan({ subsets: ["latin"] });
