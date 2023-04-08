30f8b951dd7f53a9a2d93c9c8fa93bdcdafee0de
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-04-08 08:37:25 +0300

    Add _

--- src/componnets/Booking/Form.tsx
+++ src/componnets/Booking/Form.tsx
@@ -13,6 +13,65 @@ import AddPeople from "./AddPeople";
 
 const Form = () => {
   const [addPeople, setAddPeople] = useState<number>(0);
+  const [date, setDate] = useState({
+    mm: "",
+    dd: "",
+    yyyy: "",
+  });
+  const [time, setTime] = useState({
+    hr: "",
+    min: "",
+    ammp: "",
+  });
+  const [bookingData, setBookingData] = useState({
+    name: "",
+    email: "",
+    date: "",
+    time: "",
+    people: addPeople,
+  });
+
+  const nameEmailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setBookingData({
+      ...bookingData,
+      [e.target.name]: value,
+    });
+  };
+
+  // takes the date value from 3 different input elements
+  const dateHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // turn date obj into a string
+    setDate({ ...date, [e.target.name]: e.target.value });
+
+    const dateStr = Object.values(date).join("-");
+
+    setBookingData({
+      ...bookingData,
+      date: dateStr,
+    });
+  };
+
+  // takes the date value from 2 different input elements
+  const timeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // turn date obj into a string
+    setTime({ ...time, [e.target.name]: e.target.value });
+
+    const timeStr = Object.values(time).join(":");
+
+    setBookingData({
+      ...bookingData,
+      time: timeStr,
+    });
+  };
+
+  const onSubmitHandler = () => {
+    console.log(bookingData);
+  };
+
+  console.log(time);
+  console.log(Object.values(time).join(":"));
 
   return (
     <Box
@@ -42,8 +101,20 @@ const Form = () => {
         boxShadow="1px 8px 20px -2px #919191"
         bg="#fff"
       >
-        <Input variant="flushed" pl="8px" placeholder="Name" />
-        <Input variant="flushed" pl="8px" placeholder="Email" />
+        <Input
+          variant="flushed"
+          pl="8px"
+          onChange={nameEmailHandler}
+          name="name"
+          placeholder="Name"
+        />
+        <Input
+          variant="flushed"
+          pl="8px"
+          onChange={nameEmailHandler}
+          name="email"
+          placeholder="Email"
+        />
         <Flex
           flexDirection={{ base: "column", md: "row" }}
           alignItems={{ md: "center" }}
@@ -53,9 +124,27 @@ const Form = () => {
             Pick a date
           </Text>
           <Flex gap="1rem" width={{ base: "100%", md: "50%" }}>
-            <Input variant="flushed" pl="8px" placeholder="MM" />
-            <Input variant="flushed" pl="8px" placeholder="DD" />
-            <Input variant="flushed" pl="8px" placeholder="YYYY" />
+            <Input
+              variant="flushed"
+              name="mm"
+              onChange={dateHandler}
+              pl="8px"
+              placeholder="MM"
+            />
+            <Input
+              variant="flushed"
+              name="dd"
+              onChange={dateHandler}
+              pl="8px"
+              placeholder="DD"
+            />
+            <Input
+              variant="flushed"
+              name="yyyy"
+              onChange={dateHandler}
+              pl="8px"
+              placeholder="YYYY"
+            />
           </Flex>
         </Flex>
         <Flex gap="1rem" flexDirection={{ base: "column", md: "row" }}>
@@ -63,16 +152,38 @@ const Form = () => {
             Pick a time
           </Text>
           <Flex gap="1rem" width={{ base: "100%", md: "50%" }}>
-            <Input variant="flushed" pl="8px" placeholder="09" />
-            <Input variant="flushed" pl="8px" placeholder="00" />
-            <Select variant="flushed" textAlign="center" defaultValue="AM">
+            <Input
+              variant="flushed"
+              name="hr"
+              onChange={timeHandler}
+              pl="8px"
+              placeholder="09"
+            />
+            <Input
+              variant="flushed"
+              name="min"
+              onChange={timeHandler}
+              pl="8px"
+              placeholder="00"
+            />
+            <Select
+              variant="flushed"
+              textAlign="center"
+              name="ampm"
+              onChange={(e) =>
+                setTime({ ...time, ammp: e.target.selectedOptions[0].value })
+              }
+              defaultValue="AM"
+            >
               <option value="AM">AM</option>
               <option value="PM">PM</option>
             </Select>
           </Flex>
         </Flex>
         <AddPeople setIncrement={setAddPeople} increment={addPeople} />
-        <Button variant="solid">make reservation</Button>
+        <Button type="submit" onClick={onSubmitHandler} variant="solid">
+          make reservation
+        </Button>
       </Stack>
     </Box>
   );
